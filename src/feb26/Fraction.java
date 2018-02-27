b3bbcf95bcdcf86c9fddf57e3e4009cd1c2b942d
ayusufji <mr.monk5051@gmail.com>
2018-02-27 09:03:34 -0600

    Added variable, constructor, and compareTo.

--- src/feb26/Fraction.java
+++ src/feb26/Fraction.java
@@ -1,6 +1,28 @@
 package feb26;
 
-public class Fraction
+public class Fraction implements Comparable<Fraction>
 {
+    private int num;
+    private int denom;
 
+    public Fraction(int num, int denom)
+    {
+        this.num = num;
+        this.denom = denom;
+        simplifiy();
+
+    }
+
+    @Override
+    public int compareTo(Fraction o) {
+        int diff = this.num*o.denom - o.num*this.denom;
+        if (diff > 0) return 1;
+        else if (diff < 0) return -1;
+        return 0;
+    }
+
+    private void simplifiy()
+    {
+
+    }
 }
